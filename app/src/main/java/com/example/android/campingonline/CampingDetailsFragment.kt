e9d013932212ab728d34331716d62ccd219ef4e3
YngveSDiscovery <yngve_skorge@discovery.com>
2022-02-09 16:46:53 +0200

    added compose dependencies, added first compose screen CampingDetailsFragment.kt, theming example

--- app/src/main/java/com/example/android/campingonline/CampingDetailsFragment.kt
+++ app/src/main/java/com/example/android/campingonline/CampingDetailsFragment.kt
@@ -1,35 +1,171 @@
 package com.example.android.campingonline
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.navigation.fragment.navArgs
+import com.example.android.campingonline.places.CampingSpotDetails
+import com.example.android.campingonline.ui.theme.BasicsCampingOnlineTheme
 
 class CampingDetailsFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {}
-
-        val safeArgs: CampingDetailsFragmentArgs by navArgs()
-        val campingSpotId = safeArgs.campingSpotId
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camping_details, container, false)
+    ): View {
+
+        val safeArgs: CampingDetailsFragmentArgs by navArgs()
+        val campingSpotId = safeArgs.campingSpotId
+
+        return ComposeView(requireContext()).apply {
+            setContent {
+                // TODO set our theme for the app
+                CampingDetails(campingSpotId)
+            }
+        }
     }
 
-    companion object {
-        @JvmStatic
-        fun newInstance() =
-            CampingDetailsFragment().apply {
-                arguments = Bundle().apply {}
+    @Composable
+    fun CampingDetails(campingSpotId: String) {
+        val scrollState = rememberScrollState()
+
+        BoxWithConstraints {
+            Surface {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .verticalScroll(scrollState),
+                ) {
+                    // Image header
+                    CampingDetailsHeader(
+                        campingSpotDetails = CampingSpotDetails(
+                            campingSpotId,
+                            "Miller's Point",
+                            R.drawable.caming_image_1
+                        ),
+                        containerHeight = this@BoxWithConstraints.maxHeight
+                    )
+
+                    // Content
+                    Content(
+                        "Miller's Point",
+                        this@BoxWithConstraints.maxHeight,
+                        this@BoxWithConstraints.maxWidth
+                    )
+                }
             }
+        }
+    }
+
+    @Composable
+    fun CampingDetailsHeader(campingSpotDetails: CampingSpotDetails, containerHeight: Dp) {
+        Image(
+            modifier = Modifier
+                .heightIn(max = containerHeight / 2)
+                .fillMaxWidth(),
+            painter = painterResource(id = campingSpotDetails.imageId),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+    }
+
+
+    @Composable
+    private fun Title(
+        name: String
+    ) {
+        Column(
+            modifier = Modifier.padding(
+                start = 16.dp,
+                end = 16.dp,
+                bottom = 16.dp,
+                top = 16.dp
+            )
+        ) {
+            Text(
+                text = name,
+                style = MaterialTheme.typography.h5,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+
+    @Composable
+    fun CampingProperties(label: String, value: String) {
+        Column(modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 16.dp)) {
+            Divider(modifier = Modifier.padding(bottom = 4.dp))
+            Text(
+                text = label,
+                modifier = Modifier.height(24.dp),
+                style = MaterialTheme.typography.caption,
+            )
+            Text(
+                text = value,
+                modifier = Modifier.height(24.dp),
+                style = MaterialTheme.typography.body1,
+                overflow = TextOverflow.Visible
+            )
+        }
+    }
+
+    @Composable
+    private fun Content(name: String, containerHeight: Dp, containerWidth: Dp) {
+        Column {
+            Title(name)
+            CampingProperties("Rating", "4.5")
+            CampingProperties("Description", "Description Description")
+            CampingProperties("Address", "1077 Valencia Street, Cape Town")
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .widthIn(containerWidth / 2)
+            ) {
+                Text(text = "Book now")
+            }
+        }
+
+        Spacer(Modifier.height((containerHeight - 320.dp).coerceAtLeast(0.dp)))
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultCampingDetailsPreview() {
+        CampingDetails("1")
+    }
+
+    // TODO need to be set up with porper app theme
+    @Preview(
+        showBackground = true,
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+    )
+    @Composable
+    fun DarkCampingDetailsPreview() {
+        CampingDetails("1")
     }
 }
---
+++ app/src/main/java/com/example/android/campingonline/places/CampingSpotDetails.kt
@@ -0,0 +1,8 @@
+package com.example.android.campingonline.places
+
+// TODO needs to only get
+data class CampingSpotDetails(
+    val id: String,
+    val name: String,
+    val imageId: Int
+)

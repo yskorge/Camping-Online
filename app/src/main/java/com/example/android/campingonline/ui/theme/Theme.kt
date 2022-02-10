413ac3ffa64c095b81aa289836b7e99a77a1c31b
yskorge <35079047+yskorge@users.noreply.github.com>
2022-02-10 10:20:58 +0200

    added compose dependencies, added first compose screen CampingDetailsFragment.kt, theming example (#1)
    Co-authored-by: YngveSDiscovery <yngve_skorge@discovery.com>

---
+++ app/src/main/java/com/example/android/campingonline/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+package com.example.android.campingonline.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.MaterialTheme.shapes
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+
+private val DarkColorPalette = darkColors(
+    surface = Blue,
+    onSurface = Navy,
+    primary = Navy,
+    onPrimary = Chartreuse
+)
+
+private val LightColorPalette = lightColors(
+    surface = Blue,
+    onSurface = Color.White,
+    primary = LightBlue,
+    onPrimary = Navy
+)
+
+// TODO  This is still unused and need to be adjusted according to the theme we want for this app
+// TODO this is just put here are example of how theme can be done in compose
+@Composable
+fun BasicsCampingOnlineTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}

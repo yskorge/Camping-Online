fe6db3e5697fb8593d322f4b9ea135b95414a98b
YngveSDiscovery <yngve_skorge@discovery.com>
2022-02-15 12:29:56 +0200

    styled info window, more cleanup

--- app/src/main/java/com/example/android/campingonline/MainActivity.kt
+++ app/src/main/java/com/example/android/campingonline/MainActivity.kt
@@ -2,20 +2,6 @@ package com.example.android.campingonline
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import androidx.core.content.ContextCompat
-import com.example.android.campingonline.adapters.MarkerInfoWindowAdapter
-import com.example.android.campingonline.places.CampingPlace
-import com.example.android.campingonline.places.PlacesReader
-import com.example.android.campingonline.utils.BitmapHelper
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptor
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.MarkerOptions
-import android.content.Intent
 
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/android/campingonline/adapters/MarkerInfoWindowAdapter.kt
+++ app/src/main/java/com/example/android/campingonline/adapters/MarkerInfoWindowAdapter.kt
@@ -20,24 +20,24 @@ class MarkerInfoWindowAdapter(
 ) : GoogleMap.InfoWindowAdapter {
 
     override fun getInfoContents(marker: Marker?): View? {
-        // 1. Get tag
-        val place = marker?.tag as? CampingPlace ?: return null
+        return null
+    }
 
+    override fun getInfoWindow(marker: Marker?): View? {
+        // Return null to indicate that the
+        // default window (white bubble) should be used
         // 2. Inflate view and set title, address, and rating
         val view = LayoutInflater.from(context).inflate(
-            R.layout.marker_info_contents, null
+            R.layout.marker_info_window, null
         )
+
+        //        // 1. Get tag
+        val place = marker?.tag as? CampingPlace ?: return null
 
         view.findViewById<TextView>(R.id.text_view_title).text = place.name
         view.findViewById<TextView>(R.id.text_view_address).text = place.address
         view.findViewById<TextView>(R.id.text_view_rating).text = "Rating: %.2f".format(place.rating)
 
         return view
-    }
-
-    override fun getInfoWindow(marker: Marker?): View? {
-        // Return null to indicate that the
-        // default window (white bubble) should be used
-        return null
     }
 }

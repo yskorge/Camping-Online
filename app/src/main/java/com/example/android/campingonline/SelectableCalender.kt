34f41f3e20c5fbbcc8ca9b71a86985d6261f739e
yskorge <35079047+yskorge@users.noreply.github.com>
2022-02-17 12:23:52 +0200

    First representation of unavailable dates in calendar (#3)
    * Represenation of unavailable dates in calender
    
    * TODO comment
    
    * better naming for days of calendar
    
    Co-authored-by: YngveSDiscovery <yngve_skorge@discovery.com>

--- app/src/main/java/com/example/android/campingonline/SelectableCalender.kt
+++ app/src/main/java/com/example/android/campingonline/SelectableCalender.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Card
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -13,13 +14,18 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.boguszpawlowski.composecalendar.SelectableCalendar
+import io.github.boguszpawlowski.composecalendar.day.DayState
+import io.github.boguszpawlowski.composecalendar.day.DefaultDay
 import io.github.boguszpawlowski.composecalendar.header.MonthState
 import io.github.boguszpawlowski.composecalendar.rememberSelectableCalendarState
 import io.github.boguszpawlowski.composecalendar.selection.DynamicSelectionState
@@ -36,13 +42,53 @@ fun SelectableCalendar() {
     Column(
         Modifier.verticalScroll(rememberScrollState())
     ) {
-        // TODO find a way to representate unavailabe dates
         SelectableCalendar(
             calendarState = calendarState,
             monthHeader = { CustomMonthHeader(monthState = it) },
-            weekHeader = { CustomWeekHeader(daysOfWeek = it) })
-
+            weekHeader = { CustomWeekHeader(daysOfWeek = it) },
+            dayContent = { dayState -> Days(dayState) }
+        )
         SelectionControls(selectionState = calendarState.selectionState)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun Days(dayState: DayState<DynamicSelectionState>) {
+    // Example of how the unavailable dates can be returned from the server
+    val listOfUnavailableDates = listOf("2022-02-20", "2022-02-21", "2022-02-22")
+
+    val currentDate = dayState.date.toString()
+
+    if (listOfUnavailableDates.contains(currentDate)) {
+        CustomUnavailableDay(dayState)
+    } else {
+        DefaultDay(state = dayState)
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun CustomUnavailableDay(dayState: DayState<DynamicSelectionState>, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .aspectRatio(1f)
+            .padding(2.dp),
+//        elevation = if (dayState.isFromCurrentMonth) 4.dp else 0.dp,
+//        border = if (state.isCurrentDay) BorderStroke(1.dp, currentDayColor) else null,
+//        contentColor = if (isSelected) selectionColor else contentColorFor(
+//            backgroundColor = MaterialTheme.colors.surface
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                text = dayState.date.dayOfMonth.toString(),
+                style = androidx.compose.ui.text.TextStyle(
+                    textDecoration = TextDecoration.LineThrough,
+                    // TODO With Elizabeth figure out proper theming colors here
+                    color = Color.Red
+                ),
+            )
+        }
     }
 }
 
@@ -108,18 +154,6 @@ public fun CustomWeekHeader(
         }
     }
 }
-
-//@Composable
-//fun MyDay(dayState: DayState<DynamicSelectionState>) {
-//    if (dayState.date.dayOfMonth == 1) {
-//        Text(
-//            text = dayState.date.dayOfMonth.toString(),
-//            color = Color.Green
-//        )
-//    } else {
-//        DefaultDay(state = dayState)
-//    }
-//}
 
 @Composable
 private fun SelectionControls(

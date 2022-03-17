78cae036f634ae8ad86f800ac576b3265e1730c6
yskorge <dtyskorge@gmail.com>
2022-03-17 21:42:13 +0200

    added custom compose loader animation

--- app/src/main/java/com/example/android/campingonline/fragments/PaymentFragment.kt
+++ app/src/main/java/com/example/android/campingonline/fragments/PaymentFragment.kt
@@ -5,14 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.android.campingonline.R
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import com.example.android.campingonline.animations.LoadingAnimation
+import com.example.android.campingonline.ui.theme.BasicsCampingOnlineTheme
+
 class PaymentFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment, container, false)
+        return ComposeView(requireContext()).apply {
+            setContent {
+                BasicsCampingOnlineTheme {
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        LoadingAnimation()
+                    }
+                }
+            }
+        }
     }
 }
